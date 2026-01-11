dfcada23b246a14db3a78e7386ba21b4d84d3e59
raimundosebastian-gif <raimundo.sebastian@gmail.com>
2026-01-11 19:11:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ function filtrarClientes() {
     });
 }
 
-
 // PRODUCTOS
 document.getElementById("form-producto").addEventListener("submit", guardarProducto);
 document.getElementById("producto-limpiar").addEventListener("click", limpiarFormularioProducto);
