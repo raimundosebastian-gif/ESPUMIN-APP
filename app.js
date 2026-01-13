73d7d2705c57347e4c5499c61a7ac7de245aed00
raimundosebastian-gif <raimundo.sebastian@gmail.com>
2026-01-13 11:47:18 -0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// Inserta el header.html en cada página
-fetch("header.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("header-container").innerHTML = html;
-  });
